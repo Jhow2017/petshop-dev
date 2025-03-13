8a4c37a157c885245569f767f0b02b837381386a
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-03-13 17:27:36 -0400

    att alias ds from "@ds" , create and finish componente Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { HeroSection } from '@ds/components/hero';
+
 export default function Home() {
-    return <div>ola mundo !</div>;
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    );
 }
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import { PhoneCallIcon } from 'lucide-react';
+
+//images
+import DogImage from '@ds/assets/imagens/hero-dog.webp';
+
+export function HeroSection() {
+    return (
+        <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+            <div className="">
+                <Image
+                    fill
+                    priority
+                    src={DogImage}
+                    alt="Dog hero"
+                    sizes="100vw"
+                    quality={100}
+                    className="object-cover opacity-60 lg:hidden"
+                />
+                <div className="absolute inset-0 bg-black opacity-40 lg:hidden" />
+            </div>
+            <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+                <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 ">
+                    <div className="space-y-6">
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
+                            Seu pet merece cuidado, carinho e atenção
+                        </h1>
+                        <p className="lg:text-lg">
+                            Oferecemos os melhores serviços para garantir o
+                            bem-estar e a felicidade do seu amigo de quatro
+                            patas
+                        </p>
+                        <div>
+                            <a
+                                href="#"
+                                className="bg-green-500 hover:bg-green-800 transition px-5 py-2 rounded-md font-semibold 
+                                flex items-center justify-center w-fit gap-2"
+                            >
+                                <PhoneCallIcon className="w-5 h-5" />
+                                Contato via whatsApp
+                            </a>
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-sm mb-4">
+                                <b className="bg-black text-white px-2 py-1 rounded-md">
+                                    5%
+                                </b>
+                                de desconto na primeira compra
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="hidden md:block h-full relative">
+                        <Image
+                            src={DogImage}
+                            alt="Dog hero"
+                            className="object-contain"
+                            fill
+                            sizes="(max-width: 768px) 0vw, 50vw"
+                            quality={100}
+                            priority
+                        />
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+}
