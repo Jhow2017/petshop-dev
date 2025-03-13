a695bbe33551917f84284612d5a6b068bb7f8b5d
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-03-13 18:06:24 -0400

    create ServicesSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 //sections
 import { HeroSection } from '@ds/components/hero';
 import { AboutSection } from '@ds/components/about';
+import { ServicesSection } from '@ds/components/services';
 
 export default function Home() {
     return (
         <div>
             <HeroSection />
             <AboutSection />
+            <ServicesSection />
         </div>
     );
 }
