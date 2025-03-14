41e4d97e9bc4cd8fdbbeeead2a24b8a2e6438282
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-03-13 20:11:06 -0400

    create footer

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -51,6 +51,49 @@ export function FooterSection() {
                         ))}
                     </div>
                 </div>
+
+                <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5">
+                    <div>
+                        <h3 className="text-2xl font-semibold mb-2">
+                            pet Shop Dev
+                        </h3>
+                        <p className="mb-4">
+                            Cuidando do seu melhor amigo com amor e dedicação
+                        </p>
+                        <a
+                            href="#"
+                            className="bg-green-500 px-4 py-2 rounded-md "
+                        >
+                            Contato via WhatsApp
+                        </a>
+                    </div>
+
+                    <div>
+                        <h3 className="text-2xl font-semibold mb-2">
+                            Contatos
+                        </h3>
+                        <p>Email: jhongama_jesus@hotmail.com</p>
+                        <p>Telefone: 92 99340-3737</p>
+                        <p>Rua X, centro, Manaus/Am</p>
+                    </div>
+
+                    <div>
+                        <h3 className="text-2xl font-semibold mb-2">
+                            Redes sociais
+                        </h3>
+                        <div className="flex gap-4">
+                            <a href="#" target="_blank">
+                                <FacebookLogo className="w-8 h-8" />
+                            </a>
+                            <a href="#" target="_blank">
+                                <InstagramLogo className="w-8 h-8" />
+                            </a>
+                            <a href="#" target="_blank">
+                                <YoutubeLogo className="w-8 h-8" />
+                            </a>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </section>
     );
